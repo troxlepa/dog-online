c3b5a9621723983aaf7b9c1ed42663ad00fbca0b
troxlepa <troxlepa@student.ethz.ch>
2020-05-02 10:55:12 +0200

    add firebase conf

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@ import "firebase/auth";
 import "firebase/analytics";
 
 const config = {
-  apiKey: "AIzaSyA0uguXB-erhbqFtECZLH1TOBoxi3OHEqQ",
-  authDomain: "dogonline-62b90.firebaseapp.com",
-  databaseURL: "https://dogonline-62b90.firebaseio.com",
-  projectId: "dogonline-62b90",
-  storageBucket: "dogonline-62b90.appspot.com",
-  messagingSenderId: "13254457357",
-  appId: "1:13254457357:web:173594e074d9f8d4dbfd44",
-  measurementId: "G-KWJJ59TXSV"
-  };
+  apiKey: "AIzaSyDrnuLlcoyU4-NFzaZ5H2SD8CbRITWuhZo",
+  authDomain: "dog-online-dev.firebaseapp.com",
+  databaseURL: "https://dog-online-dev.firebaseio.com",
+  projectId: "dog-online-dev",
+  storageBucket: "dog-online-dev.appspot.com",
+  messagingSenderId: "528724801441",
+  appId: "1:528724801441:web:1e603931a5929bc30b3de0",
+  measurementId: "G-PB000BT8V2"
+};
 
 
 firebase.initializeApp(config);
